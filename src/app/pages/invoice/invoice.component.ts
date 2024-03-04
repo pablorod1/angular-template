8ea863ac229e13829ae1156627aed4758c5912da
pablorod1 <158567073+pablorod1@users.noreply.github.com>
2024-03-04 18:51:49 +0100

    04/03

--- src/app/pages/invoice/invoice.component.ts
+++ src/app/pages/invoice/invoice.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-invoice',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './invoice.component.css'
 })
 export class InvoiceComponent {
+  constructor(private renderer: Renderer2){ }
 
+  printInvoice(){
+    this.renderer.selectRootElement('card').style.display = 'block'; //Muestra la tarjeta antes de imprimir
+    window.print(); //Llama a la función de impresion del navegador.
+  }
 }
